ea7b802caeb3efbbccf604bfa3190b2d266475f4
reinvdwoerd <reinvanderwoerd@me.com>
2017-09-12 17:24:51 +0200

    findPk

--- inc/resource.inc.php
+++ inc/resource.inc.php
@@ -13,6 +13,14 @@ class Resource
         $this->table = $table;
         $this->labels = $labels;
         $this->columns = $mysqli->query("DESCRIBE $table")->fetch_all();
+        $this->pk = $this->findPk($this->columns);
+    }
+
+    function findPk($columns)
+    {
+        return array_filter($columns, function($column) {
+            return $column[3] == 'PRI';
+        })[0][0];
     }
 
 
@@ -47,9 +55,9 @@ class Resource
                 echo"<td>".$row[$column]."</td>";
             }
             echo"<td>
-                    <a href=\"index.php?action=editjob&id=".$row['JobID']."\">Bewerken</a>
+                    <a href=\"index.php?action=editjob&id=".$row[$this->pk]."\">Bewerken</a>
                     |
-                    <a href=\"javascript:confirmAction('Zeker weten?', 'index.php?action=deletejob&id=".$row['JobID']."');\">Verwijderen</a>
+                    <a href=\"javascript:confirmAction('Zeker weten?', 'index.php?action=deletejob&id=".$row[$this->pk]."');\">Verwijderen</a>
                 </td>
             </tr>";
         }
@@ -86,6 +94,41 @@ class Resource
     function displayEdit()
     {
 
+        $sql = sprintf( "SELECT * FROM $his->table WHERE `JobID` = %d",
+                        $this->mysqli->escape_string($_GET['id']) );
+
+        $result = $this->mysqli->query($sql);
+
+        if($row = $result->fetch_assoc()) {
+
+            $row = escapeArray($row); // alle slashes weghalen
+
+            echo"<h1>Baan bewerken</h1>";
+
+            echo" <form method=\"post\" action=\"index.php?action=updatejob\">";
+            echo" 	<table>";
+
+            foreach ($this->labels as $column => $label) {
+                echo"		<tr>";
+                echo"			<td>$label:</td>";
+                echo"			<td><input type=\"text\" name=\"$column\" value=\"".$row[$column]."\" /></td>";
+                echo"		</tr>";
+            }
+
+            echo"		<tr>";
+            echo"			<td></td>";
+            echo"			<td><input type=\"submit\" value=\"Opslaan\" /></td>";
+            echo"		</tr>";
+            echo" 	</table>";
+
+            echo" <input type=\"hidden\" name=\"JobID\" value=\"".$row['JobID']."\" />";
+
+            echo" </form>";
+
+        }
+        else {
+            die("Geen gegevens gevonden");
+        }
     }
 
 
