1acac32dc716301e90f135827fe5210491aed566
reinvdwoerd <reinvanderwoerd@me.com>
2017-09-14 13:44:37 +0200

    <?= -> <?

--- templates/add.php
+++ templates/add.php
@@ -1,7 +1,7 @@
 <h1><?= $this->singular ?>toevoegen</h1>
 <form method="post" action="index.php?action=insert<?= $this->singular ?>">
     <table>
-        <?= include "templates/input_fields.php" ?>
-        <?= include "templates/submit_button.php" ?>
+        <? include "templates/input_fields.php" ?>
+        <? include "templates/submit_button.php" ?>
     </table>
 </form>
--- templates/edit.php
+++ templates/edit.php
@@ -1,8 +1,8 @@
-<h1><?= $this->singular ?>bewerken</h1>
- <form method="post" action="index.php?action=update<?= $this->singular ?>">
+<h1><?= $this->singular ?> bewerken</h1>
+<form method="post" action="index.php?action=update<?= $this->singular ?>">
      <table>
-        <?= include "templates/input_fields.php" ?>
-        <?= include "templates/submit_button.php" ?>
+        <? include "templates/input_fields.php" ?>
+        <? include "templates/submit_button.php" ?>
      </table>
      <input type="hidden" name="<?=$this->pk ?>" value='<?= $row[$this->pk] ?>' />
  </form>
