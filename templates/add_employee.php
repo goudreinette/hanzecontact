d5f9315dfdac00c4f9a352e22d0497467738f294
reinvdwoerd <reinvanderwoerd@me.com>
2017-09-18 15:57:13 +0200

    input fields

--- templates/add_employee.php
+++ templates/add_employee.php
@@ -1,11 +1,11 @@
 <h1>Medewerker toevoegen</h1>
 <form method="post" action="index.php?action=insertemployee" enctype="multipart/form-data">
     <table>
-        <? include "templates/input_fields.php" ?>
         <tr>
            <td>Picture:</td>
            <td> <input type="file" name="Picture"/></td>
         </tr>
+        <? include "templates/input_fields.php" ?>
         <? include "templates/submit_button.php" ?>
     </table>
 </form>
